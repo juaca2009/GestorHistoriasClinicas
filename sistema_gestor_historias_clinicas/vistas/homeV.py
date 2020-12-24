92fe149c4c10e3eb659a5f7a3fce753c3f32fcc7
juan <juaca2009@gmail.com>
2020-12-23 19:43:47 -0500

    pagina home funcional

--- sistema_gestor_historias_clinicas/vistas/homeV.py
+++ sistema_gestor_historias_clinicas/vistas/homeV.py
@@ -6,3 +6,13 @@ home_vista = Blueprint('home_vista', __name__)
 @app.route("/", methods = ["GET", "POST"])
 def home():
     return render_template("home.html")
+
+
+@app.route("/inicioSesion", methods = ["GET", "POST"])
+def inicio_sesion():
+    return render_template("login.html")
+
+
+@app.route("/registroClinico", methods = ["GET", "POST"])
+def registro_clinico():
+    return render_template("solicitud_clinica.html")
