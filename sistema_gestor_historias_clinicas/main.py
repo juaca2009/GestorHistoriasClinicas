f16ac703919b1ebe8748edc0e341ff80febd0f87
juan <juaca2009@gmail.com>
2020-12-28 02:10:14 -0500

    correccion main.py

--- sistema_gestor_historias_clinicas/main.py
+++ sistema_gestor_historias_clinicas/main.py
@@ -3,16 +3,6 @@ from vistas.homeV import home_vista
 
 
 app.register_blueprint(home_vista)
-@app.errorhandler(404)
-def not_found(error=None):
-    message = {
-        'status': 404,
-        'message': 'Not Found: ' + request.url,
-    }
-    resp = jsonify(message)
-    resp.status_code = 404
-
-    return resp
 
 if __name__ == "__main__":
     app.run(debug=True)
