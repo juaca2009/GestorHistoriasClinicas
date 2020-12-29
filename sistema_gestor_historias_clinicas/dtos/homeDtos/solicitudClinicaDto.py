6264be52ec2d2d426e5f6f9b47bfab0c8bc14218
juan <juaca2009@gmail.com>
2020-12-29 01:46:37 -0500

    creacion dto para solicitud clinica

---
+++ sistema_gestor_historias_clinicas/dtos/homeDtos/solicitudClinicaDto.py
@@ -0,0 +1,13 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SelectField
+from wtforms.validators import DataRequired, Length, Email
+
+class solicitudClinicaDto(FlaskForm):
+    email = StringField('email',
+                       validators=[DataRequired(), Email()])
+    nombreC = StringField('nombreC',
+                         validators=[DataRequired(), Length(min=2, max=20)])
+    ciudadC = SelectField('ciudadC', 
+                         coerce=str, validators=[DataRequired()])
+    direccionC = StringField('direccionC', 
+                            validators=[DataRequired(), Length(min=8, max=30)])
