304376ceb6de06d3f931b4e2ddd8f4d244aa5913
juan <juaca2009@gmail.com>
2020-12-29 01:47:29 -0500

    obtener ciudades retorna tupla-adaptacion dto

--- sistema_gestor_historias_clinicas/services/home_service.py
+++ sistema_gestor_historias_clinicas/services/home_service.py
@@ -5,9 +5,20 @@ class home_service():
         self.__HomeRepo = home_repository()
 
     def obtener_Tciudades(self):
+        """
+        funcion encargada de devolver una lista de tuplas de la forma (nombre ciudad, ciudad-departamento)
+        para se usado en los dtos correspondientes
+        SALIDAS: tuplas de la forma (nombre ciudad, ciudad-departamento)
+        """
         salida = self.__HomeRepo.obtener_ciudades()
-        ciudades_dept = list()
+        ciudades_label = list()
         for i in salida:
-            temp = i['nombre']+", "+i['departamentos.nombre']
-            ciudades_dept.append(temp)
-        return ciudades_dept
+            temp = i['nombre']+", "+i['departamentos.nombre'] #formato de nombre ciudad,departamento
+            ciudades_label.append(temp)
+        grupo_ciudades = list()
+        cont = 0
+        while cont < len(salida):
+            temp = (salida[cont]['nombre'], ciudades_label[cont]) #creacion tuplas
+            grupo_ciudades.append(temp)
+            cont = cont + 1
+        return grupo_ciudades
