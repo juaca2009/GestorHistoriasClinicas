ecdf42c6aaf6709f8af5225c3c601e9425d852d2
juan <juaca2009@gmail.com>
2021-01-05 15:15:40 -0500

    implementacion mensajes de error

--- sistema_gestor_historias_clinicas/dtos/homeDtos/solicitudClinicaDto.py
+++ sistema_gestor_historias_clinicas/dtos/homeDtos/solicitudClinicaDto.py
@@ -1,14 +1,14 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SelectField, SubmitField
-from wtforms.validators import DataRequired, Length, Email
+from wtforms.validators import DataRequired, Length, Email, InputRequired
 
 class solicitudClinicaDto(FlaskForm):
     email = StringField('email',
-                       validators=[DataRequired(), Email()])
+                       validators=[DataRequired("Rellene este campo "), Email("Ingrese un email valido")])
     nombreC = StringField('nombreC',
-                         validators=[DataRequired(), Length(min=2, max=20)])
+                         validators=[DataRequired("Rellene este campo"), Length(min=2, max=20, message="El nombre debe tener minimo 2 caracteres y maximo 20 caracteres")])
     ciudadC = SelectField('ciudadC', 
-                         coerce=str, validators=[DataRequired()])
+                         coerce=str, validators=[DataRequired("Rellene este campo")])
     direccionC = StringField('direccionC', 
-                            validators=[DataRequired(), Length(min=8, max=30)])
+                            validators=[DataRequired("Rellene este campo"), Length(min=8, max=30, message="La direccion debe tener minimo 8 caracteres y maximo 30 caracteres")])
     submit = SubmitField('Enviar Solicitud')
