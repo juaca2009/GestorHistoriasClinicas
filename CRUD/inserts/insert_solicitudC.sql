efb4c70507bfa87d46b0987f1d99381d11d278e2
juan <juaca2009@gmail.com>
2021-01-05 20:30:57 -0500

    verificacion de clinica registrada

--- CRUD/inserts/insert_solicitudC.sql
+++ CRUD/inserts/insert_solicitudC.sql
@@ -16,7 +16,7 @@ delimiter ;
 
 --funcion encargada de insertar una nueva solicitud clinica 
 --retorna 1 si se realizo la insercion
---retorna 0 si ya esa clinica posee una solicitud 
+--retorna 0 si ya esa clinica posee una solicitud  o esta creada
 delimiter //
 create function insertar_solicitudC(_nombre varchar(60), _direccion varchar(40), _ciudad varchar(50), _correo varchar(50))
     returns int
@@ -24,13 +24,21 @@ begin
     declare _cod_postal int;
     declare _id int;
     declare _nomb varchar(60);
+    declare _nomb2 varchar(60);
 
-    select codigo_postal into _cod_postal from ciudad where nombre = _ciudad;
-    select solicitud_maximo() into _id;
+   
+    
     select nombre into _nomb from solicitudes where nombre = _nombre;
     if _nomb is Null then 
-        insert into solicitudes (id, nombre, direccion, cod_postal, correo) values (_id, _nombre, _direccion, _cod_postal, _correo);
-        return 1;
+        select nombre into _nomb2 from clinicas;
+        if _nomb2 is Null then
+            select codigo_postal into _cod_postal from ciudad where nombre = _ciudad;
+            select solicitud_maximo() into _id;
+            insert into solicitudes (id, nombre, direccion, cod_postal, correo) values (_id, _nombre, _direccion, _cod_postal, _correo);
+            return 1;
+        else
+            return 0;
+        end if;
     else
         return 0;
     end if;
