3e31c9a68b4e48e9842d36d5f2c8a8b78e223495
juan <juaca2009@gmail.com>
2021-01-08 19:17:49 -0500

    script para iniciar entorno virtual

---
+++ entorno.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+iniciar_entorno(){
+	
+	cd /home/juan/Documentos/"entornos python"/gestorHEnv/bin
+	source activate
+	cd ../../../proyectos/GestorHistoriasClinicas/sistema_gestor_historias_clinicas
+}
+
+iniciar_entorno
