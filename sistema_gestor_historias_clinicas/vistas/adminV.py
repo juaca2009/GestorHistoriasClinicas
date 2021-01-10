617239d0e203f3e239812c2c366a0bf457f4a95d
juan <juaca2009@gmail.com>
2021-01-09 19:28:28 -0500

    vistas basicas admin general listas

--- sistema_gestor_historias_clinicas/vistas/adminV.py
+++ sistema_gestor_historias_clinicas/vistas/adminV.py
@@ -3,6 +3,14 @@ from app import app
 
 admin_vista = Blueprint('admin_vista', __name__)
 
-@app.route("/admin", methods = ["GET", "POST"])
-def adminHome():
+@app.route("/admin/admins", methods = ["GET", "POST"])
+def adminAdmin():
     return render_template("adminP_admin.html", titulo='Admin Sistema')
+
+@app.route("/admin/clinicas", methods = ["GET", "POST"])
+def adminClinicas():
+    return render_template("adminP_clinicas.html", titulo='Admin Sistema')
+
+@app.route("/admin/solicitudesClinicas", methods = ["GET", "POST"])
+def adminSolicitudesClinicas():
+    return render_template("adminP_solicitudes.html", titulo='Admin Sistema')
