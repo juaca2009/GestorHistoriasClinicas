d2502bc36b4c8de800464d34a37847fb42a5446e
juan <juaca2009@gmail.com>
2021-01-12 00:05:34 -0500

    no uso de sqlalchemy

--- sistema_gestor_historias_clinicas/db_config.py
+++ sistema_gestor_historias_clinicas/db_config.py
@@ -1,18 +1,17 @@
 from app import app
 from flaskext.mysql import MySQL
-from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
 
 mysql = MySQL()
 
 # MySQL configurations
 app.config['MYSQL_DATABASE_USER'] = '222146'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'juancamilo99@'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'juancamilo99'
 app.config['MYSQL_DATABASE_DB'] = 'juancamilo99_historiasc'
 app.config['MYSQL_DATABASE_HOST'] = 'mysql-juancamilo99.alwaysdata.net'
 app.config['MYSQL_DATABASE_PORT'] = 3306
+
 mysql.init_app(app)
-db = SQLAlchemy(app)
 
 # Setup the Flask-JWT-Extended extension
 app.config['JWT_SECRET_KEY'] = 'b56cde8c29107c55dce5f0ff90c59d12'
