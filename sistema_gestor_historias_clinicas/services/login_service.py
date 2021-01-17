624ed3bf0566aa6e0f7eb534207fe15bf00363d9
juan <juaca2009@gmail.com>
2021-01-17 16:37:47 -0500

    re configuracion metodos login

--- sistema_gestor_historias_clinicas/services/login_service.py
+++ sistema_gestor_historias_clinicas/services/login_service.py
@@ -2,6 +2,10 @@ from repository.adminClinicoRepository import administradorClinicoRepository
 from repository.adminGeneralRepository import administradorGeneralRepository
 from repository.medicosRepository import medicosRepository
 from repository.pacienteRepository import pacienteRepository
+from dominios.paciente import paciente
+from dominios.medicos import medicos
+from dominios.administrador_clinica import administrador_clinica
+from dominios.administrador_general import administrador_general
 
 
 
@@ -25,13 +29,17 @@ class login_servicie():
         return self.__ageneralRepo.query.filter_by(correo=_email).first()
 
     def obtener_paciente_id(self, _id):
-        return self.__pacienteRepo.query.get(int(_id))
+        pacienteTemp = paciente()
+        return pacienteTemp.query.get(int(_id))
     
     def obtener_medico_id(self, _id):
-        return self.__medicoRepo.query.get(int(_id))
+        medicoTemp = medicos()
+        return medicoTemp.query.get(int(_id))
 
     def obtener_aClinico_id(self, _id):
-        return self.__aclinicoRepo.query.get(int(_id))
+        aClinicoTemp = administrador_clinica()
+        return aClinicoTemp.query.get(int(_id))
 
     def obtener_aGeneral_id(self, _id):
-        return self.__ageneralRepo.query.get(int(_id))
+        aGeneralTemp = administrador_general()
+        return administrador_general.query.get(int(_id))
