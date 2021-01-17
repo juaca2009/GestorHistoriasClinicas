748a767e90235323b34556adc2d89e2e1d27a71c
juan <juaca2009@gmail.com>
2021-01-17 16:21:00 -0500

    instanciacion y configuracion flask login

--- sistema_gestor_historias_clinicas/app.py
+++ sistema_gestor_historias_clinicas/app.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_cors import CORS, cross_origin
+from flask_login import LoginManager
 
 app = Flask(__name__)
+login_manager = LoginManager(app)
 CORS(app)
--- sistema_gestor_historias_clinicas/main.py
+++ sistema_gestor_historias_clinicas/main.py
@@ -1,12 +1,10 @@
 from app import app
 from vistas.homeV import home_vista
 from vistas.adminV import admin_vista
-from flask_login import LoginManager
 
 
 app.register_blueprint(home_vista)
 app.register_blueprint(admin_vista)
-login_manager = LoginManager(app)
 
 if __name__ == "__main__":
     app.run(debug=True)
