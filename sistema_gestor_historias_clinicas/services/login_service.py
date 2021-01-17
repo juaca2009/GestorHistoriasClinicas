1309b0cb9d0af30d59ab49637c0ceadbf9049f0a
juan <juaca2009@gmail.com>
2021-01-17 16:25:52 -0500

    implementacion metedos para obtener modelo usuario

--- sistema_gestor_historias_clinicas/services/login_service.py
+++ sistema_gestor_historias_clinicas/services/login_service.py
@@ -1,24 +1,37 @@
-from main import login_manager
-from dominios.administrador_clinica import administrador_clinica
-from dominios.administrador_general import administrador_general
-from dominios.paciente import paciente
-from dominios.medicos import medicos
 from repository.adminClinicoRepository import administradorClinicoRepository
 from repository.adminGeneralRepository import administradorGeneralRepository
 from repository.medicosRepository import medicosRepository
 from repository.pacienteRepository import pacienteRepository
 
-@login_manager.user_loader
-def cargar_usuario(nro_documento, tipo):
-    if tipo == 'paciente':
-        pacient = paciente()
-        return pacient.query.get.(int(nro_documento))
-    elif tipo == 'medico':
-        medic = medicos()
-        return medic.query.get.(int(nro_documento))
-    elif tipo == 'aClinico':
-        aclinico = administrador_clinica()
-        return aclinico.query.get.(int(nro_documento))
-    else:
-        ageneral = administrador_general()
-        return ageneral.query.get.(int(nro_documento))
+
+
+class login_servicie():
+    def __init__(self):
+        self.__pacienteRepo = pacienteRepository()
+        self.__medicoRepo = medicosRepository()
+        self.__aclinicoRepo = administradorClinicoRepository()
+        self.__ageneralRepo = administradorGeneralRepository()
+    
+    def obtener_paciente(self, _email):
+        return self.__pacienteRepo.query.filter_by(correo=_email).first()
+
+    def obtener_medico(self, _email):
+        return self.__medicoRepo.query.filter_by(correo=_email).first()
+
+    def obtener_aclinico(self, _email):
+        return self.__aclinicoRepo.query.filter_by(correo=_email).first()
+
+    def obtener_ageneral(self, _email):
+        return self.__ageneralRepo.query.filter_by(correo=_email).first()
+
+    def obtener_paciente_id(self, _id):
+        return self.__pacienteRepo.query.get(int(_id))
+    
+    def obtener_medico_id(self, _id):
+        return self.__medicoRepo.query.get(int(_id))
+
+    def obtener_aClinico_id(self, _id):
+        return self.__aclinicoRepo.query.get(int(_id))
+
+    def obtener_aGeneral_id(self, _id):
+        return self.__ageneralRepo.query.get(int(_id))
