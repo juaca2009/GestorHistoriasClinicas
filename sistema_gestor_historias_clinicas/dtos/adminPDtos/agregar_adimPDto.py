c3a574a16d052f0e04da905b7302e58bb230c4c3
juan <juaca2009@gmail.com>
2021-01-18 13:20:52 -0500

    creacion dto agregar admin principal

---
+++ sistema_gestor_historias_clinicas/dtos/adminPDtos/agregar_adimPDto.py
@@ -0,0 +1,22 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SelectField, SubmitField
+from wtforms.validators import DataRequired, Length, Email
+
+class agregar_adimPDto(FlaskForm):
+    nombreAp = StringField('nombreAp',
+                          validators=[DataRequired("Rellene este campo"), Length(min=2, max=20, message="El nombre debe tener minimo 2 caracteres y maximo 20 caracteres")])
+    apellidosAp = StringField('apellidosAp',
+                          validators=[DataRequired("Rellene este campo"), Length(min=2, max=20, message="El nombre debe tener minimo 2 caracteres y maximo 20 caracteres")])
+    documentoAp = StringField('nombreAp',
+                          validators=[DataRequired("Rellene este campo"), Length(min=2, max=20, message="Ingrese un numero valido")])
+    TdocumentoAp = SelectField('tipoDocumento', 
+                              coerce=str, validators=[DataRequired("Rellene este campo")])
+    email = StringField('email',
+                       validators=[DataRequired("Rellene este campo "), Email("Ingrese un email valido")])
+    telefonoAp = StringField('telefonoAp',
+                          validators=[DataRequired("Rellene este campo"), Length(min=7, max=10, message="Ingrese un numero de telefono/celular valido")])
+    ciudadAp = SelectField('ciudadAp', 
+                         coerce=str, validators=[DataRequired("Rellene este campo")])
+    contrasenaAp = StringField('contrasenaAp',
+                          validators=[DataRequired("Rellene este campo"), Length(min=5, max=10, message="La contrasena debe tener minimo 5 caracteres y maximo 10 caracteres")])
+    submit = SubmitField('Registrar Administrador')
