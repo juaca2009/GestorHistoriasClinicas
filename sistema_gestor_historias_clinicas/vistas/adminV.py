4a68e70f5b3ace670d2d38b3174ebd8b10a49289
juan <juaca2009@gmail.com>
2021-01-19 13:08:16 -0500

    implementacion formulario agregar administrador

--- sistema_gestor_historias_clinicas/vistas/adminV.py
+++ sistema_gestor_historias_clinicas/vistas/adminV.py
@@ -2,10 +2,15 @@ from flask import Flask, render_template, request, redirect, session, g, url_for
 from flask_login import current_user, login_required
 from app import app
 from services.adminP_servicie import adminP_servicie
+from services.ciudad_service import ciudad_service
+from services.tipoDocumento_service import tipoDocumento_servicie
+from dtos.adminPDtos.agregar_adimPDto import agregar_adimPDto
 
 admin_vista = Blueprint('admin_vista', __name__)
 
 service_adminP = adminP_servicie()
+service_ciudad = ciudad_service()
+service_tdocumento = tipoDocumento_servicie()
 
 @app.route("/admin/admins", methods = ["GET", "POST"])
 @login_required
@@ -14,6 +19,25 @@ def adminAdmin():
         return redirect(url_for('home'))
     admins = service_adminP.obtener_administradores(session['_user_id'])
     return render_template("adminP/adminP_admin.html", titulo='Admin Sistema', admins=admins)
+
+
+
+
+@app.route("/admin/agregarAdmin", methods = ["GET", "POST"])
+@login_required
+def adminAgregar():
+    if session['tipo_cuenta'] != 'aGeneral':
+        return redirect(url_for('home'))
+    form = agregar_adimPDto()
+    ciudades = service_ciudad.obtener_Tciudades()
+    Tdocumento = service_tdocumento.obtener_Tdocumento()
+    form.ciudadAp.choices = ciudades
+    form.TdocumentoAp.choices = Tdocumento
+    return render_template("adminP/adminP_agregarAdmin.html", titulo='Admin Sistema', form=form)
+
+
+
+
 
 @app.route("/admin/clinicas", methods = ["GET", "POST"])
 @login_required
