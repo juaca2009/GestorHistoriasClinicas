20a36b4bcf291e168ce03d3de9ae4ec95444e838
juan <juaca2009@gmail.com>
2021-01-19 19:06:02 -0500

    configuracion bcrypt

--- sistema_gestor_historias_clinicas/app.py
+++ sistema_gestor_historias_clinicas/app.py
@@ -1,8 +1,13 @@
 from flask import Flask
 from flask_cors import CORS, cross_origin
 from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
+
 login_manager = LoginManager(app)
 login_manager.login_view = 'inicio_sesion'
+
+encriptador = Bcrypt()
+
 CORS(app)
