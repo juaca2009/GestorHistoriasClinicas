a42f0aed4e32acd128b4a25e917fb94657595903
juan <juaca2009@gmail.com>
2021-01-19 19:06:48 -0500

    verificacion numero de documento

--- sistema_gestor_historias_clinicas/dtos/adminPDtos/agregar_adimPDto.py
+++ sistema_gestor_historias_clinicas/dtos/adminPDtos/agregar_adimPDto.py
@@ -7,7 +7,7 @@ class agregar_adimPDto(FlaskForm):
                           validators=[DataRequired("Rellene este campo"), Length(min=2, max=20, message="El nombre debe tener minimo 2 caracteres y maximo 20 caracteres")])
     apellidosAp = StringField('apellidosAp',
                           validators=[DataRequired("Rellene este campo"), Length(min=2, max=20, message="El nombre debe tener minimo 2 caracteres y maximo 20 caracteres")])
-    documentoAp = StringField('nombreAp',
+    documentoAp = StringField('documentoAp',
                           validators=[DataRequired("Rellene este campo"), Length(min=2, max=20, message="Ingrese un numero valido")])
     TdocumentoAp = SelectField('tipoDocumento', 
                               coerce=str, validators=[DataRequired("Rellene este campo")])
@@ -20,3 +20,8 @@ class agregar_adimPDto(FlaskForm):
     contrasenaAp = StringField('contrasenaAp',
                           validators=[DataRequired("Rellene este campo"), Length(min=5, max=10, message="La contrasena debe tener minimo 5 caracteres y maximo 10 caracteres")])
     submit = SubmitField('Registrar Administrador')
+
+    def verificar_nroDocumento(self):
+        docu = str(self.documentoAp.data)
+        print(docu)
+        return docu.isdigit()
