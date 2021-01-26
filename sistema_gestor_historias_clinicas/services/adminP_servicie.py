197c905914b0f4034f710abf39beb18461f0f188
juan <juaca2009@gmail.com>
2021-01-26 17:25:27 -0500

    eliminacion metodos solicitudes

--- sistema_gestor_historias_clinicas/services/adminP_servicie.py
+++ sistema_gestor_historias_clinicas/services/adminP_servicie.py
@@ -1,12 +1,10 @@
 from repository.adminGeneralRepository import administradorGeneralRepository
-from repository.solicutudesRepository import solicitudesRepsitory
 from dominios.administrador_general import administrador_general
 from app import encriptador
 
 class adminP_servicie():
     def __init__(self):
         self.__adminPRepo = administradorGeneralRepository()
-        self.__solRepo = solicitudesRepsitory()
 
     def obtener_adminP(self, _id):
         return self.__adminPRepo.obtener_administrador(_id)
@@ -35,9 +33,5 @@ class adminP_servicie():
     def actualizar_adminP(self, _id, _ciudad, _correo, _telefono):
         salida = self.__adminPRepo.actualizar_administrador(_id, _ciudad, _correo, _telefono)
         return list(salida[0].values())
-
-    def obtener_solicitudes(self):
-        salida = self.__solRepo.obtener_solicitudes()
-        return salida
 
     
