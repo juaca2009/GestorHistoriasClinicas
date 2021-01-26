a124619fa991bfe067a2084baf0ec897ef294438
juan <juaca2009@gmail.com>
2021-01-26 17:24:37 -0500

    instanciacion services

---
+++ sistema_gestor_historias_clinicas/services/__init__.py
@@ -0,0 +1,11 @@
+from services.adminP_servicie import adminP_servicie
+from services.ciudad_service import ciudad_service
+from services.tipoDocumento_service import tipoDocumento_servicie
+from services.login_service import login_servicie
+from services.solicitudes_service import solicitudes_service
+
+service_adminP = adminP_servicie()
+service_ciudad = ciudad_service()
+service_tdocumento = tipoDocumento_servicie()
+services_login = login_servicie()
+services_solicitudes = solicitudes_service()
