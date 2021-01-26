c124590c1079a441d285d2c4ecef5d0b527311f7
juan <juaca2009@gmail.com>
2021-01-26 17:23:59 -0500

    creacion solicitudes service

---
+++ sistema_gestor_historias_clinicas/services/solicitudes_service.py
@@ -0,0 +1,14 @@
+from repository.solicutudesRepository import solicitudesRepsitory
+from dominios.solicitudes import solicitudes
+
+class solicitudes_service():
+    def __init__(self):
+        self.__solRepo = solicitudesRepsitory()
+
+    def obtener_solicitudes(self):
+        salida = self.__solRepo.obtener_solicitudes()
+        return salida
+
+    def insertar_solicitudC(self, _nombre, _direccion, _ciudad, _correo):
+        salida = self.__solRepo.insertar_solicitudC(_nombre, _direccion, _ciudad, _correo)
+        return list(salida[0].values())
