5dec00d3d0e24662dbaf7f73b82289fd34963750
juan <juaca2009@gmail.com>
2021-01-29 13:53:49 -0500

    metodo agregar admin clinico

--- sistema_gestor_historias_clinicas/repository/adminClinicoRepository.py
+++ sistema_gestor_historias_clinicas/repository/adminClinicoRepository.py
@@ -5,3 +5,16 @@ from dominios.administrador_clinica import administrador_clinica
 class administradorClinicoRepository(administrador_clinica):
     def __init__(self):
         self.__conexion = mysql.connect()
+
+    def agregar_administradorC(self, _nombre, _apellidos, _documento, _tdocumento, _email, _telefono, _clinica, _contrasena):
+        cursor = self.__conexion.cursor(pymysql.cursors.DictCursor)
+        cursor.execute(
+            """
+            select insertar_adminC(%s, %s, %s, %s, %s, %s, %s, %s)
+            """,
+            (_nombre, _apellidos, _documento, _tdocumento, _email, _telefono, _clinica, _contrasena)
+        )
+        salida = cursor.fetchall()
+        self.__conexion.commit()
+        cursor.close()
+        return salida
---
+++ sistema_gestor_historias_clinicas/services/adminC_service.py
@@ -0,0 +1,12 @@
+from repository.adminClinicoRepository import administradorClinicoRepository
+from dominios.administrador_clinico import administrador_clinico
+from app import encriptador
+
+class adminC_service():
+    def __init__(self):
+        self.__adminCRepo = administradorClinicoRepository()
+    
+    def agregar_adminC(self, _nombre, _apellidos, _documento, _tdocumento, _email, _telefono, _clinica, _contrasena):
+        _contrasena = encriptador.generate_password_hash(_contrasena).decode('utf-8')
+        salida = self.__adminCRepo.agregar_administradorC(_nombre, _apellidos, _documento, _tdocumento, _email, _telefono, _clinica, _contrasena)
+        return list(salida[0].values())
