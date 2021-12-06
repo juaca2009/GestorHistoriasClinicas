3efd1d37b0de2edff134b70e67c1ea275374648f
juan <juaca2009@gmail.com>
2021-12-06 16:06:18 -0500

    cambio ruta entornos virtual

--- entorno.sh
+++ entorno.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 iniciar_entorno(){
 	
-	cd /home/juan/Documentos/"entornos python"/gestorHEnv/bin
+	cd ~/.virtualenvs/gestorHEnv/bin
 	source activate
-	cd ../../../proyectos/GestorHistoriasClinicas/sistema_gestor_historias_clinicas
+	cd ../../../Documentos/proyectos/GestorHistoriasClinicas/sistema_gestor_historias_clinicas
 }
 
 iniciar_entorno
